b1d7ad9c0911200df451034fc26812d02d7ef860
ggldnl <danielgigliotti99.dg@gmail.com>
2024-11-09 10:50:37 +0100

    Fixed bug with robot trying to start moving when path has no elements in it | added info prints

--- demo/scripts/src/guide_me.py
+++ demo/scripts/src/guide_me.py
@@ -15,6 +15,7 @@ def guide_me(user, current_room, target_room, modim_web_server, action_manager, 
     path = position_manager.compute_path(current_room, target_room, user.alevel)
 
     if len(path) == 0:
+        print("[INFO] No route to " + target_room)
         if user.alevel == 0:    # Blindness
             modim_web_server.run_interaction(action_manager.blind_ask_call)
         else:                   # Deafness
@@ -25,17 +26,20 @@ def guide_me(user, current_room, target_room, modim_web_server, action_manager, 
             print("[INFO] Performing call to " + target_room + " room")
             modim_web_server.run_interaction(action_manager.call)
 
-    # Use the first node of the path to establish which hand to raise
-    first_room = path[0]
-    arm_picked = 'Right' if first_room.x < 0 else 'Left'
-    print("[INFO] Selected " + arm_picked.lower() + " hand to raise")
-    
-    # Create the automaton
-    robot_automaton = create_automaton(modim_web_server, action_manager, position_manager, wtime=wtime, arm=arm_picked,
-                                       alevel=user.alevel)
+    else:
+        print("[INFO] Route to " + target_room + " found")
 
-    # Start
-    robot_automaton.start('steady_state')
+        # Use the first node of the path to establish which hand to raise
+        first_room = path[0]
+        arm_picked = 'Right' if first_room.x < 0 else 'Left'
+        print("[INFO] Selected " + arm_picked.lower() + " hand to raise")
+
+        # Create the automaton
+        robot_automaton = create_automaton(modim_web_server, action_manager, position_manager, wtime=wtime, arm=arm_picked,
+                                           alevel=user.alevel)
+
+        # Start
+        robot_automaton.start('steady_state')
 
 
 if __name__ == "__main__":
